b4090f99602372e130d3012a0c1989a0a437eb6d
Michael Obst <micha@Michaels-MBP.fritz.box>
2020-02-01 21:39:26 +0100

    Initial commit

---
+++ src/main/java/com/wildcodeschool/myProjectWithSecurity/SpringSecurityQuestApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.myProjectWithSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityQuestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wildcodeschool/myProjectWithSecurity/controllers/HelloWorldController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.wildcodeschool.myProjectWithSecurity.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author micha
+ *
+ */
+//@RestController
+@Controller
+public class HelloWorldController {
+	
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!!!";
+	}
+
+}
